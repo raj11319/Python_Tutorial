ae94d5880f5e0e051b770f71476e848e6a1400cd
Raj Srivastava <raj113192007@gmail.com>
2025-07-22 09:01:42 +0530

    Update Sum.py

--- Sum.py
+++ Sum.py
@@ -1,5 +1,5 @@
 a = int(input("enter number a : "))
 b = int(input("enter number b : "))
 sum = print ("sum is ", a + b )
-#this code shows hoe the sum between two numbers work
+#this code shows how the sum between two numbers work
 #if we dont write the int before input the code will not run properly
